a8fe5f9fcab4ee414a8991942b2b86ca72c7c0d4
april-EXO <hsy.k10@gmail.com>
2025-07-25 10:54:50 +0800

    authentication api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthController;
 
+// Authentication routes (public)
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes (require authentication)
+Route::middleware('auth:sanctum')->group(function () {
+    // User routes
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout-all', [AuthController::class, 'logoutAll']);
+    Route::put('/user/country', [AuthController::class, 'updateCountry']);
+});
+
+// Public routes (no authentication required)
 // Categories
 Route::prefix('categories')->group(function () {
     Route::get('/', [CategoryController::class, 'index']);
